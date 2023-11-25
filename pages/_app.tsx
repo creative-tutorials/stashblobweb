3859dade034f7fc4dbf392855642db874461759a
creative-tutorials <treasurealekhojie@yahoo.com>
2023-11-25 17:54:53 +0100

    added analytics tracking
    Signed-off-by: creative-tutorials <treasurealekhojie@yahoo.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script src="https://cdn.splitbee.io/sb.js" />
+    </>
+    // <script async src=""></script>
+  );
 }
